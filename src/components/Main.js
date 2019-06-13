c0a51ea8f7e47defb8e303d08ddf196bedec474c
Terence Chiu <tchiu2@gmail.com>
2019-06-13 00:12:54 -0400

    add IEX attribution

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { 
+  Link,
+  Typography,
+} from '@material-ui/core';
 
 import Portfolio from './portfolio/PortfolioContainer';
 import Transactions from './transactions/TransactionsContainer';
 import Navbar from './navbar/NavbarContainer';
+
+const Attribution = () => (
+  <Typography variant="body2" color="textSecondary" align="center">
+    {'Data provided by '}
+    <Link color="inherit" href="https://iexcloud.io">
+      IEX Cloud
+    </Link>
+  </Typography>
+);
 
 const Main = () => (
   <main>
@@ -12,6 +25,7 @@ const Main = () => (
       <Route path="/portfolio" component={Portfolio} />
       <Route path="/transactions" component={Transactions} />
     </Switch>
+    <Attribution />
   </main>
 );
 
